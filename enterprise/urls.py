172cab659812af00b5271f68243177d1d63c2c3a
kingofhawks <kingofhawks@qq.com>
2014-06-20 11:34:07 +0800

    refactor change and i18n support

--- enterprise/urls.py
+++ enterprise/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'worksmart.views.home', name='home'),
     #url(r'^$', views.trend, name='trend'),
     #url(r'^list/new/$', views.new_list, name='enterprise.list.new'),
-    url(r'^submissions$', views.SubmissionList.as_view(), name='enterprise.submissions'),
+    url(r'^submissions$', views.SubmissionList.as_view(), name='enterprise.applications'),
     url(r'^projects$', views.ProjectList.as_view(), name='enterprise.projects'),
 )
