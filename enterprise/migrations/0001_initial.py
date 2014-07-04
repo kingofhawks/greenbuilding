e7c87cfbf1575eae0303e532636dee17f9d8c022
kingofhawks <kingofhawks@qq.com>
2014-07-04 11:14:11 +0800

    add lodash.js;bug fix for messenger usage

--- enterprise/migrations/0001_initial.py
+++ enterprise/migrations/0001_initial.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
@@ -14,44 +16,19 @@ class Migration(migrations.Migration):
             name='Project',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, to_field='id')),
                 ('name', models.CharField(max_length=256, verbose_name='project_name')),
-                ('location', models.CharField(max_length=256, null=True, verbose_name='location')),
-                ('description', models.CharField(max_length=1024, null=True, verbose_name='description')),
-                ('construct_company', models.CharField(max_length=256, null=True, verbose_name='construct_company')),
-                ('contact', models.CharField(max_length=128, null=True, verbose_name='contact')),
-                ('phone', models.CharField(max_length=30, null=True, verbose_name='phone')),
+                ('location', models.CharField(max_length=256, verbose_name='location', blank=True)),
+                ('description', models.CharField(max_length=1024, verbose_name='description', blank=True)),
+                ('construct_company', models.CharField(max_length=256, verbose_name='construct_company', blank=True)),
+                ('contact', models.CharField(max_length=128, verbose_name='contact', blank=True)),
+                ('phone', models.CharField(max_length=30, verbose_name='phone', blank=True)),
+                ('address', models.CharField(max_length=256, verbose_name='address', blank=True)),
+                ('zipcode', models.CharField(max_length=6, verbose_name='zipcode', blank=True)),
             ],
             options={
                 'ordering': [b'name'],
                 'verbose_name': 'project',
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='ApplicationReview',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('project', models.ForeignKey(to='enterprise.Project', to_field='id')),
-                ('grade', models.SmallIntegerField(verbose_name='grade')),
-                ('date', models.DateTimeField(verbose_name='date')),
-            ],
-            options={
-                'ordering': [b'date'],
-                'verbose_name': 'application_review',
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='Submission',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('grade', models.SmallIntegerField(verbose_name='grade')),
-                ('project', models.ForeignKey(to='enterprise.Project', to_field='id')),
-                ('date', models.DateTimeField(verbose_name='date')),
-            ],
-            options={
-                'ordering': [b'date'],
-                'verbose_name': 'submission',
             },
             bases=(models.Model,),
         ),
